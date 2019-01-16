7af02c9160ebe91f2536bdbf06962eec0e690436
Bruno Krebs <bruno.krebs@auth0.com>
2019-01-16 14:52:56 -0200

    using mongodb-memory-server;

---
+++ blog-backend/mongo.js
@@ -0,0 +1,17 @@
+const {MongoMemoryServer} = require('mongodb-memory-server');
+
+(async () => {
+  const mongod = new MongoMemoryServer({
+    instance: {
+      port: 27017,
+      dbName: 'nest-react-project',
+    }
+  });
+
+  const uri = await mongod.getConnectionString();
+  const port = await mongod.getPort();
+  const dbPath = await mongod.getDbPath();
+  const dbName = await mongod.getDbName();
+
+  console.log(uri, port, dbPath, dbName);
+})();
